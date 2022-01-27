c47f7cfde378af45afd99e6e6cf050064d2e06d7
ashtamikn <ashtamikn@gmail.com>
2022-01-27 14:20:22 +0530

    first commit

---
+++ routes/catalog.js
@@ -0,0 +1,44 @@
+const router = require("express").Router(),
+  db = require("../db/db");
+
+router.get("/", (req, res) => {
+  res.redirect("/catalog/TATA");
+});
+
+router.get("/:catalog", (req, res) => {
+  if (!req.user) return res.redirect("/");
+  // console.log(req.params.catalog)
+  console.log(req)
+  db.query(
+    `SELECT * FROM brand
+    WHERE name LIKE CONCAT('%', ?, '%');`,
+    req.params.catalog,
+    (err, result) => {
+      console.log(result,req.params)
+      db.query(
+        
+        `SELECT * FROM cars
+      WHERE brand LIKE ?
+      AND active NOT LIKE 0;`,
+        result[0].name,
+        (err1, result1) => {
+          db.query(
+            `SELECT name FROM brand
+        WHERE name NOT LIKE ?;`,
+            result[0].name,
+            (err2, result2) => {
+              return res.render("catalog", {
+                brand: result[0],
+                carsArr: result1,
+                type: result2,
+                user: req.user,
+              });
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+module.exports = router;
